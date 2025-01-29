11fad0efc646396c049bc8ee5308a33d2329e0be
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2025-01-29 17:32:55 -0300

    Add property address

--- SolutionUXComex.RegistrationOfPeople.Domain/Entities/PersonEntity.cs
+++ SolutionUXComex.RegistrationOfPeople.Domain/Entities/PersonEntity.cs
@@ -5,5 +5,12 @@
         public string Name { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string Cpf { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Number { get; set; } = string.Empty;
+        public string Complement { get; set; } = string.Empty;
+        public string Neighborhood { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+
     }
 }
---
+++ SolutionUXComex.RegistrationOfPeople.Infra/Migrations/20250129192140_MigrationInitial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SolutionUXComex.RegistrationOfPeople.Infra.Migrations
+{
+    public partial class MigrationInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
